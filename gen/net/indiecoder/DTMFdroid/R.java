5dd6457f5cba181c4d7807362bab88cadc8363a3
Marcus Kida <marcus@bitblocker.(none)>
2012-05-15 19:14:50 +0200

    Added Gfx for Buttons also added dtmf layout

--- gen/net/indiecoder/DTMFdroid/R.java
+++ gen/net/indiecoder/DTMFdroid/R.java
@@ -11,10 +11,27 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int 0=0x7f020000;
+        public static final int 1=0x7f020001;
+        public static final int 2=0x7f020002;
+        public static final int 3=0x7f020003;
+        public static final int 4=0x7f020004;
+        public static final int 5=0x7f020005;
+        public static final int 6=0x7f020006;
+        public static final int 7=0x7f020007;
+        public static final int 8=0x7f020008;
+        public static final int 9=0x7f020009;
+        public static final int back=0x7f02000a;
+        public static final int call=0x7f02000b;
+        public static final int call2=0x7f02000c;
+        public static final int contact=0x7f02000d;
+        public static final int dash=0x7f02000e;
+        public static final int ic_launcher=0x7f02000f;
+        public static final int r1=0x7f020010;
+        public static final int r2=0x7f020011;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int dtmf=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
